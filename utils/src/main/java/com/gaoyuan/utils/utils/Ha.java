f11898419a7bffb0b370147976bc0bc84f7d06e6
gaoyuan <1179074755@qq.com>
2017-03-12 23:29:57 +0800

    测试

--- utils/src/main/java/com/gaoyuan/utils/utils/Ha.java
+++ utils/src/main/java/com/gaoyuan/utils/utils/Ha.java
@@ -1,11 +1,13 @@
 package com.gaoyuan.utils.utils;
 
+import android.content.Context;
+
 /**
  * Created by gaoyuan on 2017/3/12.
  */
 
 public class Ha {
-    public static void ha(){
-
+    public static void ha(Context context){
+        ToastUtils.showToast(context,"hah");
     }
 }
